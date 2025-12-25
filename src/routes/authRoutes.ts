1af413b1e34c41087b8373d465161482aeb13258
dslee0525 <lds8835@gmail.com>
2025-12-25 23:13:28 +0900

    feat:auth api 관련해서 Swagger 설정 #1

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,7 +4,54 @@ import { authenticateToken } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/auth/signup:
+ *   post:
+ *     summary: 관리자 회원가입
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               organizationName:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: 성공
+ */
 router.post("/signup", signup);
+
+/**
+ * @swagger
+ * /api/auth/signin:
+ *   post:
+ *     summary: 로그인
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: 성공
+ */
 router.post("/signin", signin);
 
 router.get("/home", authenticateToken, (req, res) => {
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import express from "express";
 import type { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
 import authRoutes from "./routes/authRoutes.js";
+// swagger 관련
+import swaggerUi from "swagger-ui-express";
+import { specs } from "./config/swagger.js";
 
 // 환경 변수 로드
 dotenv.config();
@@ -14,6 +16,8 @@ const PORT = process.env.PORT || 4000;
 // 미들웨어 설정
 app.use(cors()); // 다른 도메인(Next.js)에서의 요청 허용
 app.use(express.json()); // JSON 바디 파싱
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // 기본 헬스체크 라우트
 app.get("/", (req: Request, res: Response) => {
